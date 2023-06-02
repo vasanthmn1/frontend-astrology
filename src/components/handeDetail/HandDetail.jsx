9e2b53d3e4673ec3264c65885f144941dfe58822
vasanthmn1 <mnvasanth4@gmail.com>
2023-06-02 12:42:23 +0530

    detail

---
+++ src/components/handeDetail/HandDetail.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import classes from './handDetail.module.css'
+import { Col, Container, Row } from 'react-bootstrap'
+import img from '../../../assets/hand.png'
+const HandDetail = () => {
+    return (
+        <div className={classes.container}>
+            <Container>
+                <Row>
+                    <Col lg="6">
+                        <div className={classes.img}>
+                            <img src={img} />
+                        </div>
+                    </Col>
+                    <Col lg="6">
+                        <div className={classes.detail}>
+                            <h2>
+                                Tarot is a powerful tool for transformation
+                            </h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mi tellus, pulvinar vel tempus eget, finibus vitae ante. Fusce sit amet velit eleifend, iaculis velit quis, malesuada lacus. Vestibulum sodales magna a volutpat tempus. Mauris vestibulum id urna viverra ultrices. Nullam rhoncus elit eget libero varius dapibus.
+                            </p>
+                            <button>Get started</button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default HandDetail
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,16 +2,18 @@ import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import Banner from '../../components/banner/Banner'
 import Spiner from '../../components/spiner/Spiner'
-
+import HandDetail from '../../components/handeDetail/HandDetail'
+import classes from './home.module.css'
 
 const Home = () => {
 
 
     return (
-        <>
+        <div className={classes.conatiner}>
             <Banner />
             {/* <Spiner /> */}
-        </>
+            <HandDetail />
+        </div>
 
     )
 }
